8fff3aa1e3d79df063e5b0395da334417ebfd781
kvarkonyi@gmail.com <katelyn@Silver.local>
2017-09-14 19:40:42 -0500

    Studio part one (random movies)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +10,7 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    tomorrow_movie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
@@ -16,14 +19,18 @@ def index():
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
-    # the heading "<h1>Tommorrow's Movie</h1>"
+    content += "<h1>Tommorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrow_movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movies = ["The Big Lebowski", "Ghost in the Shell", "Pacific Rim", "Lord of the Rings - Fellowship of the Ring", "Moana"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    return random.choice(movies)
 
 
 app.run()
